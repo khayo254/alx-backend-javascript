1e53c1eeb3a0067a1e145549975c5227d338659c
khayo254 <khayo1998@gmail.om>
2024-04-27 01:50:36 +0300

    backend js

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,16 @@
+export function taskFirst() {
+    const task = 'I prefer const when I can.'; // Using const to instantiate the variable
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    let combination = 'But sometimes let'; // Using let to instantiate the variable
+    combination += getLast();
+  
+    return combination;
+  }
+  
---
+++ 0x00-ES6_basic/eslint.config.mjs
@@ -0,0 +1,54 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+
+export default [
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+];
+
+{
+  "parser": "babel-eslint",
+  "extends": ["eslint:recommended"],
+  "rules": {
+    // your rules
+  }
+}
+
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
